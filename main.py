2416aa267471c6cf1f9fbeb1d59809a4afe108ff
Tran Thuc Minh Tri <minhtri170603@gmail.com>
2023-12-03 19:48:21 +0700

    Draw path function
    Use this function to draw your solution path for each algorithm.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Tile import Tile
 import pygame
+import time
 import math
 
 height = 0
@@ -59,34 +60,48 @@ TILE_HEIGHT = math.floor(SCREEN_HEIGHT / height)
 font = pygame.font.Font("HighlandGothicFLF.ttf", 20)
 
 # Initializing surface
-surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def draw_state(tiles):
+    surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-for i in range(height):
-    for k in range(width):
-        text = font.render(str(tiles[i][k].value), True, BLACK)
-        color = WHITE
-        if tiles[i][k].distance == 0:
-            color = RED
-        elif tiles[i][k].type == -1:
-            color = GRAY
-        elif tiles[i][k].type == 2:
-            color = YELLOW
-        elif tiles[i][k].type == 3:
-            color = ORANGE
-        elif tiles[i][k].type == 4:
-            color = BLUE
-        pygame.draw.rect(surface, color, pygame.Rect(TILE_WIDTH * k, TILE_HEIGHT * i + 2, TILE_WIDTH - 2, TILE_HEIGHT - 2))
-        if tiles[i][k].type == 2 or tiles[i][k].type == 3:
-            text_width = text.get_rect().width
-            text_height = text.get_rect().height
-            text_x = (TILE_WIDTH * k + TILE_WIDTH * (k + 1)) / 2 - text_width / 2
-            text_y = (TILE_HEIGHT * i + TILE_HEIGHT * (i + 1)) / 2 - text_height / 2
-            surface.blit(text, (text_x, text_y))
+    for i in range(height):
+        for k in range(width):
+            text = font.render(str(tiles[i][k].value), True, BLACK)
+            color = WHITE
+            if tiles[i][k].distance == 0:
+                color = RED
+            elif tiles[i][k].type == -1:
+                color = GRAY
+            elif tiles[i][k].type == 2:
+                color = YELLOW
+            elif tiles[i][k].type == 3:
+                color = ORANGE
+            elif tiles[i][k].type == 4:
+                color = BLUE
+            pygame.draw.rect(surface, color, pygame.Rect(TILE_WIDTH * k, TILE_HEIGHT * i + 2, TILE_WIDTH - 2, TILE_HEIGHT - 2))
+            if tiles[i][k].type == 2 or tiles[i][k].type == 3:
+                text_width = text.get_rect().width
+                text_height = text.get_rect().height
+                text_x = (TILE_WIDTH * k + TILE_WIDTH * (k + 1)) / 2 - text_width / 2
+                text_y = (TILE_HEIGHT * i + TILE_HEIGHT * (i + 1)) / 2 - text_height / 2
+                surface.blit(text, (text_x, text_y))
+    pygame.display.flip() # compulsory
 
-# Drawing Rectangle
-pygame.display.flip()
+def draw_path(tile_state, path):
+    draw_state(tiles)
+    for i in range(len(path)):
+        time.sleep(0.1)
+        tiles[path[i][0]][path[i][1]].distance = 0
+        draw_state(tiles)
+
+# Path: coordinates of the cells gone traveled through, in the correct order
+example_path = [[1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [1, 10],
+                [2, 10], [3, 10], [4, 10], [5, 10], [6, 10], [7, 10], [8, 10], [9, 10],
+                [10, 10], [11, 10], [12, 10], [12, 9], [12, 8], [12, 7]]
+# Call draw_path to color the solution path (in the same form as example_path)
+# The instructor requires that the path be shown step-by-step
+draw_path(tiles, example_path)
+
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
